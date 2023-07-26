7713b3b2bd3751e217142d94019fcbaaffa35954
Sherov Eldor <eldor.sherov.97@inbox.ru>
2023-07-26 09:52:30 +0500

    Fixed getting polygons and rendering them

--- src/static/scripts/control.js
+++ src/static/scripts/control.js
@@ -4,17 +4,14 @@ const elArrowLeft = document.querySelector('.arrow-left')
 const elArrowRight = document.querySelector('.arrow-right')
 
 
-elPanelBtn.addEventListener('click', ()=> {
+elPanelBtn.addEventListener('click', () => {
     elPanelBox.classList.toggle('paneLeft-show')
     elPanelBtn.classList.toggle('paneLeft__bnt-show')
-    if(elPanelBtn.classList.contains('paneLeft__bnt-show')){
+    if (elPanelBtn.classList.contains('paneLeft__bnt-show')) {
         elArrowLeft.style.display = 'block'
         elArrowRight.style.display = 'none'
     } else {
         elArrowRight.style.display = 'block'
         elArrowLeft.style.display = 'none'
     }
-
-    
 })
-
--- src/static/scripts/show.fields.js
+++ src/static/scripts/show.fields.js
@@ -1,50 +1,68 @@
 // =================== VARIABLES ================== //
-let polygonsLayer = null
+
 let fields__properties = []
+let checked__field__props = []
 
 
 // =================== GET HTML ELEMENTS ================== //
-
+let crop_checkBox = document.querySelectorAll('.field__crop-checkbox');
 
 // =================== GET USER FIELDS ================== //
 function get_user_fields() {
     fetch("/api/polygon/get")
         .then(res => res.json())
         .then(res => {
-            let featureCollection = res
-            featureCollection.features.forEach(feature => {
-                fields__properties.push(feature.properties)
-            })
-            polygonsLayer = L.geoJSON(featureCollection).addTo(map);
-            map.fitBounds(polygonsLayer.getBounds())
-            make_fields_list(fields__properties)
+            if (res) {
+                addToMapDrawnLayers(res)
+                make_fields_list(fields__properties)
+            } else {
+                console.log('Polygonlar yo`q');
+            }
         })
 }
 get_user_fields()
 
+
+
+var polygons_layer = new L.FeatureGroup();
+function addToMapDrawnLayers(featureGroup){
+    featureGroup.features.forEach(feature => {
+        let coordinates = feature.geometry.coordinates
+        coordinates[0][0].forEach(coor=>{
+            coor.reverse()
+        })
+        var polygon = L.polygon(coordinates, {color: 'red'}).addTo(map);
+        polygons_layer.addLayer(polygon)
+    })
+    map.fitBounds(polygons_layer.getBounds())
+}
+
+
 // =================== SEARCH BY FIELD NAME ================== //
 function searchFieldName(e) {
-    let one_field_prop = fields__properties.filter(prop => prop.place_name.toLowerCase().includes(e))
-    make_fields_list(one_field_prop)
+    if (checked__field__props.length != 0) {
+        let one_field_prop = checked__field__props.filter(prop => prop.place_name.toLowerCase().includes(e))
+        make_fields_list(one_field_prop)
+    } else if (e.length > 0) {
+        let one_field_prop = fields__properties.filter(prop => prop.place_name.toLowerCase().includes(e))
+        make_fields_list(one_field_prop)
+    } else if (e.length == 0) {
+        searchByCropName()
+    }
+
 }
 
 
 // =================== SEARCH BY CROP NAME ================== //
-let crop_checkBox = document.querySelectorAll('.field__crop-checkbox');
-
-function searchByCropName(e) {
-    console.log(fields__properties);
-    let fields_props = []
+function searchByCropName() {
+    let checked__field__props = []
     crop_checkBox.forEach((checkbox) => {
-        if(checkbox.checked){
-            console.log(checkbox.value);
-            console.log(fields__properties.filter(prop => prop.crop_code == checkbox.value));
-            fields_props.push(fields__properties.filter(prop => prop.crop_code == checkbox.value))
+        if (checkbox.checked) {
+            checked__field__props.push(...(fields__properties.filter(prop => prop.crop_code == checkbox.value)))
         }
-        
+
     });
-    // make_fields_list(fields_props[0])
-    console.log(fields_props);
+    make_fields_list(checked__field__props)
 }
 
 
