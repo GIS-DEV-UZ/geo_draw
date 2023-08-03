2d87f5663daa6b6e89eda101bc15cf2ca723e63f
Sherov Eldor <eldor.sherov.97@inbox.ru>
2023-08-03 14:03:40 +0500

    fixed config file

--- src/config/config.py
+++ src/config/config.py
@@ -1,16 +1,18 @@
+from decouple import config
+
 class Config():
-    SECRET_KEY = '2ae0a61787937e86297484b3051424dc'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:eladmin@127.0.0.1:5432/geo-draw'
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
     
-    ONEID_LOGIN = "at_agrosanoat_markazi"
-    ONEID_PASSWORD = "nCeV5BFAcruvPxW9sK721qdR"
-    ONEID_URL = "https://sso.egov.uz/sso/oauth/Authorization.do" 
+    ONEID_LOGIN = config('ONEID_LOGIN')
+    ONEID_PASSWORD = config('ONEID_PASSWORD')
+    ONEID_URL = config('ONEID_URL')
     
-    HOST = '0.0.0.0'
-    PORT = 5001
+    HOST = config('HOST')
+    PORT = config('PORT', cast=int)
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://geo_draw_admin:Geo-draw2023@blurcode.uz:5432/geo_draw'
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
 
 class TestConfig(Config):
     pass
